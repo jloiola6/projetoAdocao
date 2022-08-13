88598297e8193875ca33a8e43f101c04c3aed7ba
João Teixeira <jloiola6@outlook.com>
2022-08-13 16:48:51 -0500

    Primeira atualização

--- adocao/adocao/settings.py
+++ adocao/adocao/settings.py
@@ -120,8 +120,12 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
